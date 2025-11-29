0acccf08591d75bc0ad71dbdc50bb5be6f7fb127
tahatuzel <taha.tuzel@outlook.com>
2025-11-29 20:14:21 +0300

    edited edit distance recursive

--- EditDistanceRecursive.cpp
+++ EditDistanceRecursive.cpp
@@ -1,30 +1,29 @@
 #include "EditDistanceRecursive.h"
-
 #include <string>
 #include <algorithm>
 #include <vector>
 #include <initializer_list> 
 
-int edit_distance_recursive(std::string str1, std::string str2) {
-    const auto m = str1.length();
-    const auto n = str2.length();
 
-    if (m == 0) {
-        return n;
-    }
-
-    if (n == 0) {
-        return m;
-    }
+int edit_distance_engine(const std::string& str1, const std::string& str2, int m, int n) {
+    if (m == 0) return n;
+    if (n == 0) return m;
 
     if (str1[m - 1] == str2[n - 1]) {
-        return edit_distance_recursive(str1.substr(0, m - 1), str2.substr(0, n - 1));
+        return edit_distance_engine(str1, str2, m - 1, n - 1);
     }
 
-    const auto insert_op  = edit_distance_recursive(str1, str2.substr(0, n - 1));
-    const auto remove_op  = edit_distance_recursive(str1.substr(0, m - 1), str2);
-    const auto replace_op = edit_distance_recursive(str1.substr(0, m - 1), str2.substr(0, n - 1));
-
-    return 1 + std::min({insert_op, remove_op, replace_op});
+    return 1 + std::min({
+        edit_distance_engine(str1, str2, m, n - 1),    // Insert
+        edit_distance_engine(str1, str2, m - 1, n),    // Remove
+        edit_distance_engine(str1, str2, m - 1, n - 1) // Replace
+    });
 }
 
+
+int edit_distance_recursive(std::string str1, std::string str2) {
+    int m = str1.length();
+    int n = str2.length();
+
+    return edit_distance_engine(str1, str2, m, n);
+}
